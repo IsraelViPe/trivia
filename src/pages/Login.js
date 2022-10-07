9d9b3f06e4bdaaff9a39cf608478c43acad48223
Ernani Ávila <ernanicavilaneto@gmail.com>
2022-10-07 16:06:43 -0300

    done req 1 in pairprogramming with group

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,14 +4,53 @@ import { connect } from 'react-redux';
 import logo from '../trivia.png';
 
 class Login extends React.Component {
-  state = { };
+  state = {
+    name: '',
+    email: '',
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
 
   render() {
+    const { name, email } = this.state;
     return (
       <div className="App">
         <header className="App-header">
           <img src={ logo } className="App-logo" alt="logo" />
-          <p>SUA VEZ</p>
+          <label htmlFor="name">
+            Nome
+            <input
+              onChange={ this.handleChange }
+              data-testid="input-player-name"
+              type="text"
+              name="name"
+              id="name"
+              value={ name }
+            />
+          </label>
+          <label htmlFor="email">
+            Email
+            <input
+              onChange={ this.handleChange }
+              data-testid="input-gravatar-email"
+              type="text"
+              name="email"
+              id="email"
+              value={ email }
+            />
+          </label>
+          <button
+            disabled={ !name || !email }
+            data-testid="btn-play"
+            type="button"
+          >
+            Enviar
+
+          </button>
         </header>
       </div>
     );
